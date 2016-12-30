d8196d04c1d2efad829e0de7481f6e5938d55466
Asterisk <kuro5858tube@gmail.com>
2016-12-30 15:32:11 +0900

    #2 Exception Applied
    #2 Exception Applied - Form1.cs

---
+++ ConvertToCSV/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ConvertToCSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ConvertButton = new System.Windows.Forms.Button();
+            this.SavedPath = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.PathLabel = new System.Windows.Forms.Label();
+            this.FileNameLabel = new System.Windows.Forms.Label();
+            this.WorkName = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // ConvertButton
+            // 
+            this.ConvertButton.Font = new System.Drawing.Font("メイリオ", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.ConvertButton.Location = new System.Drawing.Point(670, 199);
+            this.ConvertButton.Name = "ConvertButton";
+            this.ConvertButton.Size = new System.Drawing.Size(200, 65);
+            this.ConvertButton.TabIndex = 0;
+            this.ConvertButton.Text = "Convert";
+            this.ConvertButton.UseVisualStyleBackColor = true;
+            this.ConvertButton.Click += new System.EventHandler(this.ConvertButton_Click);
+            // 
+            // SavedPath
+            // 
+            this.SavedPath.Location = new System.Drawing.Point(80, 21);
+            this.SavedPath.Name = "SavedPath";
+            this.SavedPath.Size = new System.Drawing.Size(787, 23);
+            this.SavedPath.TabIndex = 1;
+            this.SavedPath.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            // 
+            // PathLabel
+            // 
+            this.PathLabel.AutoSize = true;
+            this.PathLabel.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.PathLabel.ForeColor = System.Drawing.Color.Aqua;
+            this.PathLabel.Location = new System.Drawing.Point(12, 21);
+            this.PathLabel.Name = "PathLabel";
+            this.PathLabel.Size = new System.Drawing.Size(62, 24);
+            this.PathLabel.TabIndex = 2;
+            this.PathLabel.Text = "Path : ";
+            // 
+            // FileNameLabel
+            // 
+            this.FileNameLabel.AutoSize = true;
+            this.FileNameLabel.Font = new System.Drawing.Font("メイリオ", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.FileNameLabel.ForeColor = System.Drawing.Color.Aqua;
+            this.FileNameLabel.Location = new System.Drawing.Point(12, 61);
+            this.FileNameLabel.Name = "FileNameLabel";
+            this.FileNameLabel.Size = new System.Drawing.Size(72, 24);
+            this.FileNameLabel.TabIndex = 3;
+            this.FileNameLabel.Text = "Name : ";
+            // 
+            // WorkName
+            // 
+            this.WorkName.Location = new System.Drawing.Point(80, 61);
+            this.WorkName.Name = "WorkName";
+            this.WorkName.Size = new System.Drawing.Size(787, 24);
+            this.WorkName.TabIndex = 4;
+            this.WorkName.Text = "";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.BackColor = System.Drawing.Color.Teal;
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 276);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(882, 22);
+            this.statusStrip1.TabIndex = 5;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(55, 17);
+            this.toolStripStatusLabel1.Text = "準備完了";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(882, 298);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.WorkName);
+            this.Controls.Add(this.FileNameLabel);
+            this.Controls.Add(this.PathLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.SavedPath);
+            this.Controls.Add(this.ConvertButton);
+            this.Name = "Form1";
+            this.Text = "ConvertCSV Ver.1.0.0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ConvertButton;
+        private System.Windows.Forms.RichTextBox SavedPath;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label PathLabel;
+        private System.Windows.Forms.Label FileNameLabel;
+        private System.Windows.Forms.RichTextBox WorkName;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ ConvertToCSV/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace ConvertToCSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            return;
+        }
+
+        private void ConvertButton_Click(object sender, EventArgs e)
+        {
+            DialogResult result = MessageBox.Show( "ファイルを作成しますか？",
+                                                   "ファイル作成！",
+                                                   MessageBoxButtons.YesNoCancel,
+                                                   MessageBoxIcon.Exclamation,
+                                                   MessageBoxDefaultButton.Button2
+                                                  );
+
+            switch ( result )
+            {
+                case DialogResult.Yes:
+                    executeCsv(); // CSV 書き込みメソッドコール
+                    break;
+
+                case DialogResult.No:
+                    break;
+
+                default:
+                    break;
+
+            }
+
+            return;
+        }
+
+        /// <summary>
+        /// CSV 書き込みメソッド
+        /// </summary>
+        private void executeCsv()
+        {
+            // ボタンクリック時の現在時刻を取得
+            var time = DateTime.Now;
+            var data = WorkName.Text; 
+
+            // CSV用 にテストデータを用意
+            var WorkData = new List<List<string>>()
+            {
+                new List<string>(){ data }
+            };
+
+            // ボタンクリック時の時刻をリストに追加
+            var date = new List<List<DateTime>>()
+            {
+                new List<DateTime>(){ time }
+            };
+
+            try
+            {
+                // 保存する Path を取得
+                string Path = SavedPath.Text;
+
+                // .csv ファイルを生成し、データを書き込む
+                using (var wr = new CsvWriter( Path ))
+                {
+                    try
+                    {
+                        wr.Write(date);
+                        wr.Write(WorkData);
+                    }
+                    catch (ArgumentException arg)
+                    {
+                        // スタックトレース
+                        Console.WriteLine(arg.StackTrace);
+                    }
+                    finally
+                    {
+                        // none
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // スタックトレース
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                // none
+            }
+
+            #region ※コメントアウト
+            /* 1 Record Read */
+            /*
+            List<string> row = null;
+            using (var csv = new CsvReader(@"TestData.csv"))
+            {
+                while ((row = csv.ReadRow()) != null)
+                {
+                    Console.WriteLine(row[0].ToString());
+                }
+            }
+            */
+
+            /* All Read */
+            /*
+            List<List<string>> data = null;
+
+                using (var csv = new CsvReader(@"TestData.csv"))
+                {
+                    data = csv.ReadToEnd();
+                }
+            }
+            */
+            #endregion
+
+            return;
+        }
+    }
+}
